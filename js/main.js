3aa06a2f52cb0883425d663f12ee6008a1c9c275
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-03 14:59:14 +0200

    semi, ex working, refining needed

---
+++ js/main.js
@@ -0,0 +1,34 @@
+class User {
+    constructor (name, surname, favColor, pw) {
+        this.name = name;
+        this.surname = surname;
+        this.favColor = favColor;
+        this.pw = pw;
+    }
+}
+
+// var u1 = new User("testname", "testsurname", "testcolor");
+
+var u1 = new User();
+
+// name
+
+u1.name = prompt("what's your name?");
+
+// surname
+
+u1.surname = prompt("what's your surname?");
+
+// fav color
+
+u1.favColor = prompt("what's your favourite color?");
+
+// password
+
+u1.pw = String(`${u1.name}${u1.surname}${u1.favColor}23`);
+
+alert(`ur ai generated password is ${pw}`);
+
+console.log(u1.name + "  " + u1.surname + "  " + u1.favColor + "  " + "23");
+
+document.getElementById('user').innerHTML = `${u1.name}, ${u1.surname}, ${u1.favColor} 23 `;
